600c6466b214d1a8a55f30be90f3985fdc930a82
Harold Joshua Holder <haroldjholder@gmail.com>
2021-08-26 13:31:48 -0700

    Added More comments

--- File Extensions Add or Change/AddExtension.cs
+++ File Extensions Add or Change/AddExtension.cs
@@ -10,40 +10,43 @@ namespace File_Extensions_Add_or_Change
 {
     class AddExtension
     {
-        string extensionName { get; set; }
-        string textBoxedFilled { get; set; }
-        string FilePathOfSelectedFiles { get; set; }
+        string extensionName { get; set; } //Gets the Extension that was typed in or selected from dropbox
+        string textBoxedFilled { get; set; } //Gets the user input from textbox
+        string FilePathOfSelectedFiles { get; set; } //Gets the File Path of selected Files
 
-        List<string> FullPathOfAllFiles = new List<string>();
-        List<string> selectedFileNames = new List<string>();
-        List<string> ViewAllChangedFiles = new List<string>();
+        List<string> FullPathOfAllFiles = new List<string>(); //Holds all Path and file names of all selected files
+        List<string> selectedFileNames = new List<string>(); //Holds the File names
+        List<string> ViewAllChangedFiles = new List<string>(); //Holds file names and extensions
 
-        static Form1 form1 = System.Windows.Forms.Application.OpenForms.OfType<Form1>().FirstOrDefault();
+        static Form1 form1 = System.Windows.Forms.Application.OpenForms.OfType<Form1>().FirstOrDefault(); //Holds current instance of Form1 so able to  
+                                                                                                          //use items from it without starting new
 
         
-        public void ThisIsTheFIlePathOfFiles(string dirName, string fulllPath, string fileNameWithExtOnly)
+        public void ThisIsTheFIlePathOfFiles(string dirName, string fulllPath, string fileNameWithExtOnly)//Method to pass values for use
         {
-            FilePathOfSelectedFiles = dirName;
-            FullPathOfAllFiles.Add(fulllPath);
-            ViewAllChangedFiles.Add(fileNameWithExtOnly);
+            FilePathOfSelectedFiles = dirName; //Directory Name
+            FullPathOfAllFiles.Add(fulllPath); //Full path including directory nanme and ext
+            ViewAllChangedFiles.Add(fileNameWithExtOnly); //FIle name and extension only
 
         }
 
-        public void StartTheChange()
+        public void StartTheChange() //Method to rename the file with the chosen extension
         {
             try
             {
-                DisableTheButtons();
+                DisableTheButtons(); //Call method to disablebuttons so not able to change after file extension has been renamed
 
-                for (int i = 0; i < selectedFileNames.Count; i++)
+                for (int i = 0; i < selectedFileNames.Count; i++) //Loop through all selected files
                 {
                     form1.listView4.Items.Add(ViewAllChangedFiles[i]); //update listview with all files changed
 
-                    File.Move(FullPathOfAllFiles[i], FilePathOfSelectedFiles + "\\" + selectedFileNames[i] + extensionName, true);
-                    form1.listView3.Items.Add(selectedFileNames[i] + extensionName);
+                    File.Move(FullPathOfAllFiles[i], FilePathOfSelectedFiles + "\\" + selectedFileNames[i] + extensionName, true); //Use the Move method to rename 
+                                                                                                                                   //the file and put in same location
+
+                    form1.listView3.Items.Add(selectedFileNames[i] + extensionName); //Display the file with the new extension
                 }
-                FullPathOfAllFiles.Clear();
-                selectedFileNames.Clear();
+                FullPathOfAllFiles.Clear(); //Make sure list is now empty
+                selectedFileNames.Clear(); //make sure list is now empty
                 
             }
             catch (Exception ex)
@@ -53,73 +56,76 @@ namespace File_Extensions_Add_or_Change
         }
 
 
-       public List<string> CheckFileNameExtension()
+       public List<string> CheckFileNameExtension() //Checks if the file has an extension or not
         {
-            List<string> altered = new List<string>();
+            List<string> altered = new List<string>(); //LIst to hold changed files
 
-            for (int i = 0; i < selectedFileNames.Count; i++)
+            for (int i = 0; i < selectedFileNames.Count; i++) //Loop through all selected files
             {
-                if(selectedFileNames[i].Contains("."))
+                if(selectedFileNames[i].Contains(".")) //Check if the current selected file has a "." 
                 {
-                    int index = selectedFileNames[i].IndexOf(".");
-                    StringBuilder sb = new StringBuilder(selectedFileNames[i]);
+                    int index = selectedFileNames[i].IndexOf("."); //Get the current index of the "." and store its value
 
-                    sb.Remove(index, selectedFileNames[i].Length - index);
+                    StringBuilder sb = new StringBuilder(selectedFileNames[i]); //Use instance of stringbuilder for each file
+
+                    sb.Remove(index, selectedFileNames[i].Length - index); //Remove the "." from the file that has it & start it at the position of where it is seen
+                                                                           //Remove everything after the start of it so to delete the extension as well
                    
-                    altered.Add(sb.ToString());
+                    altered.Add(sb.ToString()); //Add all new files to the List
                 }
                 else
                 {
-                    altered.Add(selectedFileNames[i]);
+                    altered.Add(selectedFileNames[i]); //If no "." in file name add file to the altered List
                 }
             }
 
-            selectedFileNames = altered;
+            selectedFileNames = altered; //Pass the altered List back to the selectedFileNames List
 
-            return selectedFileNames;
+            return selectedFileNames; //Return the List back for use
         }
 
 
-        public string CheckTheString()
+        public string CheckTheString() //Checks if the user input follows the rules
         {
-            CheckFileNameExtension();
+            CheckFileNameExtension(); //Call method to check the file name extension
 
-            textBoxedFilled = '.' + form1.textBox1.Text;
+            textBoxedFilled = '.' + form1.textBox1.Text; //Add a "." just to be sure there is one included if the file has no "."
 
-            if(textBoxedFilled[1] == '.')
+            if(textBoxedFilled[1] == '.') //IF there is a "." in the 2nd position (index 1) of the string the do the following
             {
-                StringBuilder sb = new StringBuilder(textBoxedFilled);
+                StringBuilder sb = new StringBuilder(textBoxedFilled); //Create an instance of the stringbuilder for textBoxedFilled string
 
-                sb.Remove(0, 1); //This will remove one character starting at index 1 of the string
+                sb.Remove(0, 1); //This will remove one character starting at index 0 of the string
+                                 //Becaues if in this if statement then there must be 2 occurrences of "."
 
-                textBoxedFilled = sb.ToString();
+                textBoxedFilled = sb.ToString(); //Take new string and send to string textBoxedFIlled
 
-                form1.button2.Enabled = true;
+                form1.button2.Enabled = true; //Activate the button2 (Start Button)
               
-                return textBoxedFilled;
+                return textBoxedFilled; //Send back the new string
             }
 
-            else
+            else //Check if the user entered a ".", if not send a warning
             {
                 MessageBox.Show("You must use a '.' before declaring your extension.");
-                form1.textBox1.Clear();
+                form1.textBox1.Clear(); //Erase what the user entered to start over
             }
 
-            return "";
+            return ""; //Return an empty string if wrong input was entered
         }
 
-        public void ExtensionAddition(string str)
+        public void ExtensionAddition(string str) //Method to get the chose extension
         {
-            extensionName = str;
-            CombineThem();
+            extensionName = str; //Store the extension
+            CombineThem(); //Call Method 
         }
 
-        public void NameOfFIles(List<string> str)
+        public void NameOfFIles(List<string> str) //Store the list of files
         {
             selectedFileNames = str;
         }
 
-        public void CombineThem()
+        public void CombineThem() //Method that displays the current selected files with the chosen extension name
         {
             form1.listView2.Items.Clear();
             foreach (string file in selectedFileNames)
@@ -128,7 +134,7 @@ namespace File_Extensions_Add_or_Change
             }
         }
 
-       static public void DisableTheButtons()
+       static public void DisableTheButtons() //Method that disables all the buttons and comboboxe 
         {
             form1.radioButton1.Enabled = false;
             form1.radioButton1.Checked = false;
@@ -141,7 +147,7 @@ namespace File_Extensions_Add_or_Change
             form1.button2.Enabled = false;
         }
 
-        public void ClearTheScreen()
+        public void ClearTheScreen() //Method that clears everything that is non important for the current task
         {
             FullPathOfAllFiles.Clear();
             selectedFileNames.Clear();
