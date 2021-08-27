bc106c11e0cb3893f5a4279731b02021a56b0e87
Harold Joshua Holder <haroldjholder@gmail.com>
2021-08-27 00:55:25 -0700

    Added an icons and settings for publish with oneclick

--- File Extensions Add or Change/Form1.Designer.cs
+++ File Extensions Add or Change/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace File_Extensions_Add_or_Change
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
@@ -262,6 +263,7 @@ namespace File_Extensions_Add_or_Change
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Add Or Change Extension Name";
